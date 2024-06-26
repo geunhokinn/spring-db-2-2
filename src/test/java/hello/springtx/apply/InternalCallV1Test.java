af133b80fbf9a66cb277870fbdcec7a48cbe8c28
geunhokinn <gghh1220@gmail.com>
2024-06-26 10:42:40 +0900

    test: 트랜잭션 프록시 내부 호출 테스트 1

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,66 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    // 메서드에 어노테이션이 걸려있기 때문에 프록시가 주입됨
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            // 트랜잭션이 필요 없는 영역
+            log.info("call external");
+            printTxInfo();
+            internal();
+        }
+
+        // 트랜잭션이 필요한 영역
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
